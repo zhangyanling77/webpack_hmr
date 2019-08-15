90a60a815e64904aec8f78d7af7dde9e60f1fc39
zhangyanling77 <18783226594@163.com>
2019-08-15 10:18:57 +0800

    Create src.index.js

---
+++ src.index.js
@@ -0,0 +1,16 @@
+import './client.js'
+
+
+var root = document.getElementById('root')
+
+function render() {
+  let title = require('./title').default
+
+  root.innerHTML = title
+}
+
+render()
+
+if(module.hot){
+  module.hot.accept(['./title.js'], render)
+}
